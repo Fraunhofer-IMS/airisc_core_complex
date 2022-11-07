ee4427971d1c047d022c0af95590ff1d87fe109f
stnolting <stnolting@gmail.com>
2022-11-07 06:43:26 +0100

    [.ci] add Icarus Verilog simulation scripts

---
+++ .ci/iverilog_sim.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd $(dirname "$0")
+
+TOP_DIR=${TOP_DIR:-../.}
+
+cd "$TOP_DIR"/tb
+
+iverilog -v \
+  -DCONFIG_IDEAL_SRAM_1 \
+  -DSIM \
+  -I "$TOP_DIR"/tb \
+  -I "$TOP_DIR"/tb/tests \
+  -I "$TOP_DIR"/src \
+  -o "$TOP_DIR"/.ci/airi5c-sim \
+  -c "$TOP_DIR"/.ci/sim_file_list.txt
+
+vvp "$TOP_DIR"/.ci/airi5c-sim
