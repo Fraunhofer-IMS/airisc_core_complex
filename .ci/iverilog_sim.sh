f6e188e9c2008695e856dba1005d46e751ae92a2
Carsten Rolfes <carsten.rolfes@ims.fraunhofer.de>
2023-06-02 13:25:29 +0200

    update of files

--- .ci/iverilog_sim.sh
+++ .ci/iverilog_sim.sh
@@ -8,6 +8,10 @@ TOP_DIR=${TOP_DIR:-../.}
 
 cd "$TOP_DIR"/tb
 
+# remove VHDL includes (they are not relevant for
+# this minimal iverilog simulation)
+patch -b "$TOP_DIR"/src/modules/airi5c_trng/src/airi5c_trng.v "$TOP_DIR"/.ci/airi5c_trng.v.patch
+
 iverilog -v \
   -DCONFIG_IDEAL_SRAM_1 \
   -DSIM \
